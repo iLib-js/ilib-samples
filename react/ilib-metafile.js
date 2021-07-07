e84d72f115b0c3e5f8c522fadd21ed789f8359b3
Goun Lee <goun.lee@lge.com>
2021-07-07 10:55:09 +0900

    Update config for ilib test

---
+++ react/ilib-metafile.js
@@ -0,0 +1,14 @@
+var ilib = require("ilib/lib/ilib.js");
+
+// assign each class to a subproperty of "ilib"
+ilib.Locale = require("ilib/lib/Locale.js");
+ilib.DateFmt = require("ilib/lib/DateFmt.js");
+ilib.NumFmt = require("ilib/lib/NumFmt.js");
+
+// This unpacks the above classes to the global scope
+require("ilib/lib/ilib-unpack.js");
+
+// Must be at the end of meta file to generate the locale data files
+require("ilib/lib/ilib-getdata.js");
+
+module.exports = ilib;
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,6 +1,16 @@
+const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const IlibWebpackPlugin = require("ilib-webpack-plugin");
 const port = process.env.PORT || 3000;
+
+// this goes outside the module.export
+let options = {
+    locales: ["en-US", "de-DE", "fr-FR", "it-IT", "ja-JP", "ko-KR", "zh-Hans-CN"],
+    assembly: "dynamicdata",
+    compilation: 'compiled',
+    size: 'custom',
+    tempDir: 'assets'
+};
 
 module.exports = {
   mode: 'development',
@@ -8,6 +18,15 @@ module.exports = {
   output: {
     filename: 'bundle.[hash].js',
   },
+  entry: path.resolve("./ilib-metafile.js"),
+  output: {
+    filename: 'ilib-custom.js',
+        chunkFilename: 'ilib.[name].js',  // to name the locale bundles
+        path: path.resolve("./output"), // choose an appropriate output dir
+        publicPath: "output/",              // add the corresponding URL
+        library: 'ilib',
+        libraryTarget: 'umd'
+    },
   module: {
     rules: [
       {
@@ -28,12 +47,20 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(js|html)$/,
+        use: {
+          loader: "ilib-webpack-loader",
+          options: options
+        }
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
     }),
+    new IlibWebpackPlugin(options)
   ],
   devServer: {
     host: 'localhost',
