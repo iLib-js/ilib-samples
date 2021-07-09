034ffa47f3c8d96e843147ecc68502de5017005f
Goun Lee <gouni6020@gmail.com>
2021-07-09 14:36:55 +0900

    tab to spaces

--- enact/src/App/App.js
+++ enact/src/App/App.js
@@ -5,35 +5,35 @@ import {connect} from 'react-redux';
 import MainPanel from '../views/MainPanel';
 
 class App extends Component {
-	constructor (props) {
-		super(props);
+    constructor (props) {
+        super(props);
 
-		if (typeof document !== 'undefined') {
-			document.addEventListener('webOSLocaleChange', function () {
-				document.location.reload();
-				console.log('webOSLocaleChange');
-			}, false);
-		}
-	}
+        if (typeof document !== 'undefined') {
+            document.addEventListener('webOSLocaleChange', function () {
+                document.location.reload();
+                console.log('webOSLocaleChange');
+            }, false);
+        }
+    }
 
-	onClose = () => {
-		if (window) {
-			window.close();
-		}
-	};
+    onClose = () => {
+        if (window) {
+            window.close();
+        }
+    };
 
-	render () {
-		const {...props} = this.props;
-		delete props.dispatch;
+    render () {
+        const {...props} = this.props;
+        delete props.dispatch;
 
-		return (
-			<div {...props}>
-				<Panels onClose={this.onClose}>
-					<MainPanel />
-				</Panels>
-			</div>
-		);
-	}
+        return (
+            <div {...props}>
+                <Panels onClose={this.onClose}>
+                    <MainPanel />
+                </Panels>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = ({locale}) => ({locale});
