61e9235b1c9136e7624ad023cd7e1e206a93b57e
Goun Lee <goun.lee@lge.com>
2024-02-16 16:09:37 +0900

    Update script and remove appList.txt

--- lint-webos/script/execute.sh
+++ lint-webos/script/execute.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
 START_TIME=$(date +%s)
-cat appList.txt | while read app
-#cat appListSimple.txt | while read app
+
+#DIR = /home/goun/Source/swp/localization-data
+DIR=$1
+cd $DIR
+echo $PWD
+for appDir in `find . -type d`
 do
-    
+  if [ "$appDir" != "." ]; then
+    cd $appDir
+    echo "<<< " $appDir " >>>"
+    node ~/Source/ilib-lint/src/index.js -c /home/goun/Source/swp/lintScript/ilib-lint-config.json -i
+    cd ..
+    echo "===========================================================================\n\n"
+  fi
+done
+
+echo "It took $(($END_TIME - $START_TIME)) seconds to check xliff files of all app ..."
+echo "----------------- Done -- --------------- "
+
+:<<'END'
+cat appList.txt | while read app
+do
     echo "$app"
     cd ../localization-data
     if [ -d "$app" ]; then
@@ -18,3 +36,4 @@ END_TIME=$(date +%s)
 
 echo "It took $(($END_TIME - $START_TIME)) seconds to check xliff files of all app ..."
 echo "----------------- Done -- --------------- "
+END
