3e8875d52f6fe9a96a98764411c822a8e0027177
Goun Lee <goun.lee@lge.com>
2024-02-19 11:12:58 +0900

    Update script

--- lint-webos/script/execute.sh
+++ lint-webos/script/execute.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
+: <<'END'
+DIR = /home/goun/Source/swp/localization-data
+CONFIG = /home/goun/Source/swp/lintScript/ilib-lint-config.json
+./execute.sh ~/Source/swp/localization-data/ ~/Source/swp/lintScript/ilib-lint-config.json 
+END
 
-#DIR = /home/goun/Source/swp/localization-data
-DIR=$1
+DIR=${1?param missing - Specify the localization-data path }
+CONFIG=${2?param missing - Specify the ilib-llint-config.json file path }
 cd $DIR
 echo $PWD
 appCnt=0
 invalidCnt=0
+START_TIME=$(date +%s)
+arrInvalidDir=()
 for appDir in `find . -type d`
 do
   if [ "$appDir" == "." ]; then
-    invalidCnt=$((invalidCnt+1))
-    echo $appDir " Invalid Directory"
-  elif [ "$appDir" == "./vid" ]; then
-    invalidCnt=$((invalidCnt+1))
-    echo $appDir " Invalid Directory"
-  elif [ "$appDir" == "./Enact" ]; then
-    invalidCnt=$((invalidCnt+1))
-    echo $appDir " Invalid Directory"
-  elif [ "$appDir" == "./chromecast" ]; then
-    invalidCnt=$((invalidCnt+1))
-    echo $appDir " Invalid Directory"
+    arrInvalidDir+=($appDir)
   elif [ "$appDir" == "./git" ]; then
-        invalidCnt=$((invalidCnt+1))
-    echo $appDir " Invalid Directory"
+    arrInvalidDir+=($appDir)
+  elif [ "$appDir" == "./vid" ]; then
+    arrInvalidDir+=($appDir)
+  elif [ "$appDir" == "./Enact" ]; then
+    arrInvalidDir+=($appDir)
+  elif [ "$appDir" == "./chromecast" ]; then
+    arrInvalidDir+=($appDir)
   elif [ "$appDir" == "Framework" ]; then
-    invalidCnt=$((invalidCnt+1))
-    echo $appDir " Invalid Directory"
+    arrInvalidDir+=($appDir)
   elif [ "$appDir" == "built-in" ]; then
-    invalidCnt=$((invalidCnt+1))
-    echo $PWD
-    echo $appDir " Invalid Directory"
+    arrInvalidDir+=($appDir)
   else
     cd $appDir
     appCnt=$((appCnt+1))
     echo "<<< ("$appCnt")" $appDir " >>>"
-    node ~/Source/ilib-lint/src/index.js -c /home/goun/Source/swp/lintScript/ilib-lint-config.json -i
+    node ~/Source/ilib-lint/src/index.js -c $CONFIG -i
     cd ..
-    echo "===========================================================================\n\n"
+    echo "==========================================================================="
   fi
 done
-echo "<<< ("invalidCnt")" $invalidCnt " >>>"
+echo "<<< ("invalidCnt")" ${#arrInvalidDir[@]} " >>>"
+for value in "${arrInvalidDir[@]}"
+do
+  echo "[" $value "] "
+done
 
 END_TIME=$(date +%s)
 
 echo "It took $(($END_TIME - $START_TIME)) seconds to check xliff files of all app ..."
-echo "----------------- Done -- --------------- "
+echo "----------------- Done ------------------"
